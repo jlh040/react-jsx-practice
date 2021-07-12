4edf6808f0705fafc0906c76d7b52b8a15095e47
Jeff Hall <jeff.leon.hall@gmail.com>
2021-07-12 16:50:21 -0700

    modified the App component

--- part-2/App.js
+++ part-2/App.js
@@ -6,17 +6,27 @@ const App = () => {
                 name="Mark" 
                 date="07/04/2021" 
                 message="This is my first tweet" />
+            <hr />
             <Tweet 
                 username="hatperson54" 
                 name="Dirk" 
                 date="08/03/1999" 
-                message="I'm going to the mall today, hopefully I can find the shoes that I wan't to buy,
+                message="I'm going to the mall today, hopefully I can find the shoes that I want to buy,
                 otherwise I will be very angry" />
+            <hr />
             <Tweet 
                 username="anon786" 
-                name="jeff" 
+                name="Jeff" 
                 date="12/21/2016" 
                 message="Hey, who's going to the party tonight?" />
+            <hr />
+            <Tweet 
+                username="big_bob99" 
+                name="Bob"
+                message="Tonight, I shall chop wood" />
+            <hr />
         </div>
     )
 };
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- part-2/Tweet.js
+++ part-2/Tweet.js
@@ -1,10 +1,10 @@
 let today = new Date();
 let todayReadable = today.toDateString();
 
-const Tweet = ({username="N/A", name="N/A", date={todayReadable}, message="N/A"}) => {
+const Tweet = ({username="N/A", name="N/A", date=todayReadable, message="N/A"}) => {
     return (
         <div>
-            <p><b>Tweet:</b></p>
+            <p><b><u>Tweet</u></b></p>
             <p><b>Username: </b>{username}</p>
             <p><b>Name: </b>{name}</p>
             <p><b>Message: </b>{message}</p>
