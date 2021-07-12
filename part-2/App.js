2d186eaffa2fdc452d9194ec9f28a723ed01e88a
Jeff Hall <jeff.leon.hall@gmail.com>
2021-07-12 16:45:04 -0700

    created the App component

---
+++ part-2/App.js
@@ -0,0 +1,22 @@
+const App = () => {
+    return (
+        <div>
+            <Tweet 
+                username="dog_guy22" 
+                name="Mark" 
+                date="07/04/2021" 
+                message="This is my first tweet" />
+            <Tweet 
+                username="hatperson54" 
+                name="Dirk" 
+                date="08/03/1999" 
+                message="I'm going to the mall today, hopefully I can find the shoes that I wan't to buy,
+                otherwise I will be very angry" />
+            <Tweet 
+                username="anon786" 
+                name="jeff" 
+                date="12/21/2016" 
+                message="Hey, who's going to the party tonight?" />
+        </div>
+    )
+};
