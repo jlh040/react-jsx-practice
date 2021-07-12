1b4ac9bcb45f8781dd47195fb1a48290fef0bfa2
Jeff Hall <jeff.leon.hall@gmail.com>
2021-07-12 16:37:36 -0700

    created the Tweet component

---
+++ part-2/Tweet.js
@@ -0,0 +1,13 @@
+let today = new Date();
+let todayReadable = today.toDateString();
+
+const Tweet = ({username="N/A", name="N/A", date={todayReadable}, message="N/A"}) => {
+    return (
+        <div>
+            <p><b>Username: </b>{username}</p>
+            <p><b>Name: </b>{name}</p>
+            <p><b>Message: </b>{message}</p>
+            <p><b>Date: </b>{date}</p>
+        </div>
+    )
+};
