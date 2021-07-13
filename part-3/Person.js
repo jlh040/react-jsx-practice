d997e36635ea781b73d9d01c32db54070633317c
Jeff Hall <jeff.leon.hall@gmail.com>
2021-07-12 17:33:45 -0700

    modified the Person component

--- part-3/Person.js
+++ part-3/Person.js
@@ -3,7 +3,7 @@ const Person = ({name, age, hobbies}) => {
         <div>
             <p>Learn some information about this person</p>
             <p><b>Name: </b> {name.length > 8 ? name.slice(0,6) : name}</p>
-            <h3>{age >= 18 ? 'please go vote!' : 'you must be 18'}</h3>
+            <h3>Age: {age} ({age >= 18 ? 'please go vote!' : 'you must be 18'})</h3>
             <p><b>Hobbies:</b></p>
             <ul>
                 {hobbies.map(hobby => <li>{hobby}</li>)}
