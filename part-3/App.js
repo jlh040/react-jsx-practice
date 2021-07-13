7b45d99f084c4a7e2ba5d8a89a095c79b58a04fa
Jeff Hall <jeff.leon.hall@gmail.com>
2021-07-12 17:30:18 -0700

    modified the App component

--- part-3/App.js
+++ part-3/App.js
@@ -4,15 +4,20 @@ const App = () => {
             <Person 
                 name="Bob" 
                 age={17} 
-                hobbies={['Biking, Going to the mall, Swimming with friends']} />
+                hobbies={['Biking', 'Going to the mall', 'Swimming with friends']} />
+            <hr />
             <Person 
                 name="Michaelangelo" 
                 age={35} 
                 hobbies={['Buying lucrative and expensive furniture']} />
+            <hr />
             <Person 
                 name="John" 
                 age={18} 
                 hobbies={['Taking care of my grandma', 'Having lunch with family', 'Being a great person']} />
+            <hr />
         </div>
     )
 };
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- part-3/Person.js
+++ part-3/Person.js
@@ -4,9 +4,9 @@ const Person = ({name, age, hobbies}) => {
             <p>Learn some information about this person</p>
             <p><b>Name: </b> {name.length > 8 ? name.slice(0,6) : name}</p>
             <h3>{age >= 18 ? 'please go vote!' : 'you must be 18'}</h3>
+            <p><b>Hobbies:</b></p>
             <ul>
-                Hobbies:
-                {hobbies.map(hobby => <li>hobby</li>)}
+                {hobbies.map(hobby => <li>{hobby}</li>)}
             </ul>
         </div>
     )
